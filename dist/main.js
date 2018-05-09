e24f62e9dbe05e975d14c298dc0a774d578f7262
foma <fomenkoyegor@gmail.com>
2018-05-09 19:14:22 +0300

    final

--- dist/main.js
+++ dist/main.js
@@ -5,6 +5,7 @@ window.addEventListener("load", function () {
     var content = document.querySelector("#content");
     var quotes = document.querySelector(".output .quotes");
 
+    //загрузить заметки она же обновляет при удалении и при добавлении
     function getQuotes() {
         quotes.innerHTML = "";
         fetch("https://limitless-citadel-88557.herokuapp.com/api/quotes").then(function (res) {
@@ -17,8 +18,10 @@ window.addEventListener("load", function () {
         });
     }
 
+    //взять все заметки при загрузки
     getQuotes();
 
+    //добавление
     form.addEventListener("submit", function (e) {
         e.preventDefault();
         console.log(content.value);
@@ -37,5 +40,26 @@ window.addEventListener("load", function () {
         }).catch(function (res) {
             console.log(res);
         });
+    });
+
+    //удаление
+
+    quotes.addEventListener("click", function (e) {
+        if (e.target.matches(".del")) {
+            fetch("https://limitless-citadel-88557.herokuapp.com/api/quote/" + e.target.dataset.id, {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: "DELETE"
+
+            }).then(function (res) {
+                console.log(res);
+                alert("Quote   deleted!!!");
+                getQuotes();
+            }).catch(function (res) {
+                console.log(res);
+            });
+        }
     });
 });
--- src/main.js
+++ src/main.js
@@ -3,29 +3,29 @@ window.addEventListener("load", function () {
     const content = document.querySelector("#content");
     const quotes = document.querySelector(".output .quotes");
 
+//загрузить заметки она же обновляет при удалении и при добавлении
     function getQuotes() {
-        quotes.innerHTML="";
-        fetch("https://limitless-citadel-88557.herokuapp.com/api/quotes").
-            then(res => res.json()).
-            then(data => {
-                let res=data.quotes;
-                res.forEach(function (user) {
-                    quotes.innerHTML+=
-                        `    <li class="flex jcb p-5 mt-2">
+        quotes.innerHTML = "";
+        fetch("https://limitless-citadel-88557.herokuapp.com/api/quotes").then(res => res.json()).then(data => {
+            let res = data.quotes;
+            res.forEach(function (user) {
+                quotes.innerHTML +=
+                    `    <li class="flex jcb p-5 mt-2">
                                  <p>${user.content}</p> 
                                   <button class="del" data-id="${user.id}">del</button> 
                              </li>
                             
                         `
-                })
-
-
             })
-         }
 
+
+        })
+    }
+
+//взять все заметки при загрузки
     getQuotes();
 
-
+//добавление
     form.addEventListener("submit", (e) => {
         e.preventDefault();
         console.log(content.value);
@@ -47,5 +47,29 @@ window.addEventListener("load", function () {
             .catch(function (res) {
                 console.log(res)
             })
+    });
+
+    //удаление
+
+    quotes.addEventListener("click", (e) => {
+        if (e.target.matches(".del")) {
+            fetch(`https://limitless-citadel-88557.herokuapp.com/api/quote/${e.target.dataset.id}`,
+                {
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    method: "DELETE",
+
+                })
+                .then(function (res) {
+                    console.log(res);
+                    alert(`Quote   deleted!!!`);
+                    getQuotes();
+                })
+                .catch(function (res) {
+                    console.log(res)
+                })
+        }
     })
 });
